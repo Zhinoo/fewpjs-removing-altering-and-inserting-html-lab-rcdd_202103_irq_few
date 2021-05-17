f3ac7a58060be2f312ddc54e2324dc23520f15fb
Zhino Salam <zhinosalam188@gmail.com>
2021-05-17 14:35:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,24 +1,8 @@
 // Write your code here!
+et main = document.querySelector('main#main')
 
-// var element = document.createElement('div');
-// document.body.appendChild(element);
+main.remove()
 
-// var ul = document.createElement('ul');
-
-// for (let i = 0; i < 3; i++) 
-//     {
-//     let li = document.createElement('li');
-//     li.innerHTML = (i + 1).toString();
-//     ul.appendChild(li);
-//     }
-
-// element.appendChild(ul);
-
-document.getElementById('main').remove();
-
-var newHeader = document.createElement('h1');
-newHeader.setAttribute("id","victory");
-newHeader.innerHTML = "LUKKAAAA is the champion";
-// innerText = document.createTextNode("LUKAAAAA is the champion");
-// newHeader.appendChild(innerText);
-document.body.appendChild(newHeader);
+let newHeader = document.createElement('h1')
+newHeader.id = "victory"
+newHeader.innerHTML = "Danny is the champion"
